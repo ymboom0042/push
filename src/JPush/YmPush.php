1745a570197e637aacf4427e2286cbd1934c2c1b
ymboom0042 <362006539@qq.com>
2020-11-06 17:55:53 +0800

    新增广播

--- src/JPush/YmPush.php
+++ src/JPush/YmPush.php
@@ -1,27 +1,30 @@
 <?php
+
 namespace JPush;
 
 class YmPush
 {
-    private $conf;
-    public function __construct( array $conf )
+    private $conf = [];
+
+    public function __construct(array $conf)
     {
-        $this -> conf = $conf;
+        if (empty($this->conf)) {
+            $this->conf = $conf;
+        }
     }
 
+
     /**
-     * 极光推送 安卓ios数据分离
-     * @param array $push_id 推送用户push_id [1,2,3]
+     * 极光推送 --数据
      * @param array $content 推送内容
-     * @param string $platform 推送平台
-     * @return bool
+     * @return array
      */
-    function push( array $push_id, array $content,  string $platform = 'all' ) : bool
+    private function pushData(array $content): array
     {
-        if (!empty($push_id) && !empty($content)) {
+        if (!empty($content)) {
             $ios = [
                 "alert" => [
-                    "body"  => $content["msg"],
+                    "body" => $content["msg"],
                     "title" => $content["title"],
                 ],
                 "extras" => [
@@ -32,54 +35,85 @@ class YmPush
 
             $android = [
                 "alert" => $content["msg"],
-                "data"  => [
+                "data" => [
                     "extras" => [
                         "sign" => "sign"
                     ],
                 ],
             ];
 
-            return $this -> JPush($push_id, $ios, $android,$platform);
+            return [$ios, $android];
         }
-        return false;
+        return [];
     }
 
 
     /**
-     * 极光推送
+     * 极光推送 --指定用户
      * @param array $push_id
-     * @param array $ios
-     * @param array $android
-
+     * @param array $content
      * @param string $platform
      * @return bool
      */
-    private  function JPush( array $push_id, array $ios, array $android,string $platform) : bool
+    function pushRegisterId(array $content, array $push_id, string $platform = 'all'): bool
     {
-        $client = new Client($this -> conf["key"], $this -> conf["secret"]);
-        $push = $client -> push();
+        $client = new Client($this->conf["key"], $this->conf["secret"]);
+        $push = $client->push();
+        [$ios, $android] = $this->pushData($content);
 
-        // 是用于防止 api 调用端重试造成服务端的重复推送而定义的一个推送参数
-        $cid = $push -> getCid(1);
-        $cid = $cid['body']['cidlist'][0] ?? 0;
-        $message['msg_content'] = $android['alert'];
-        $response = $push->setPlatform($platform)
-            ->setCid($cid)
-//                    ->setAudience('all')
-//                   ->setNotificationAlert($message)
-            ->androidNotification($android['alert'], $android['data'])
-            ->iosNotification($ios['alert'], $ios)
-            ->addRegistrationId($push_id)
-            ->setOptions()
-            ->message($message)
-            ->send();
-        if (!empty($response)) {
-            $http_code = $response["http_code"] ?? false;
-            if ( $http_code == 200 ) {
-                return true;
+        if (!empty($ios) && !empty($android)) {
+            // 是用于防止 api 调用端重试造成服务端的重复推送而定义的一个推送参数
+            $cid = $push->getCid(1);
+            $cid = $cid['body']['cidlist'][0] ?? 0;
+            $message['msg_content'] = $android['alert'];
+            $response = $push->setPlatform($platform)
+                ->setCid($cid)
+                ->androidNotification($android['alert'], $android['data'])
+                ->iosNotification($ios['alert'], $ios)
+                ->addRegistrationId($push_id)
+                ->setOptions()
+                ->message($message)
+                ->send();
+
+            if (!empty($response)) {
+                $http_code = $response["http_code"] ?? false;
+                if ($http_code == 200) {
+                    return true;
+                }
             }
         }
 
+        return false;
+    }
+
+    /**
+     * 极光推送 -- 广播
+     * @param array $content
+     * @param string $platform
+     * @return bool
+     */
+    function pushAll(array $content, string $platform = 'all'): bool
+    {
+        $client = new Client($this->conf["key"], $this->conf["secret"]);
+        $push = $client->push();
+        [$ios, $android] = $this->pushData($content);
+        if (!empty($ios) && !empty($android)) {
+            $message['msg_content'] = $android['alert'];
+            $response = $push->setPlatform($platform)
+                ->setAudience('all')
+                ->androidNotification($android['alert'], $android['data'])
+                ->iosNotification($ios['alert'], $ios)
+                ->setOptions()
+                ->message($message)
+                ->send();
+
+            if (!empty($response)) {
+                $http_code = $response["http_code"] ?? false;
+                if ($http_code == 200) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
